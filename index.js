6083c60deb8b029f045ec9c9382dc4ee9cc3a25c
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-04-01 00:50:19 +0100

    Board factory
    I've made a factory function inside an IIFE that
    can represent a data board made from an array. I
    also have a function to place a 'playertoken'
    currently a placeholder onto that array.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // Ok. I'm using a factory function to create an object that holds the board as well as methods I can us on the board.
-// I've also wrapped the factory in an IIFE to stop it from being called again.
-(function Gameboard() {
+// I've also wrapped the factory in an IIFE so it isn't global or reusable.
+const board = (function () {
     const board = [[0,0,0],[0,0,0],[0,0,0]];
-    console.log(board);
+    // console.log(board);
 
-    // Now I need functions that operate on this board
-    
+    const playerToken = 1;
+
+    const get = () => console.log(board);
+
+    const pick = (row, column) => board[(row-1)].splice((column-1), 1, playerToken);
+
+    return {get, pick};
 })();
+
+// const playerOne = '1';
+// const playerTwo = '2';
+
+// function Players() {
+
+// };
+// I'll need players and a way of discerning who's turn it is and switching turns, also a 'game controller' 
