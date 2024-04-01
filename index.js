ad8d3ff6b2be1be2c58cb1d00c765112d424e2f4
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-04-01 19:09:23 +0100

    cellValue function
    Added a method to gameboard to get the cellValue
    which I will then use to check if said cell has
    been chosen or not when picked.

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const board = (function () {
     const board = [[0,0,0],[0,0,0],[0,0,0]];
 
-    const get = () => console.log(board);
+    const print = () => console.log(board);
 
+    const cellValue = (row, column) => {return board[row-1][column-1]};
+    
     const pick = (row, column) => {board[(row-1)].splice((column-1), 1, players.getToken());
-        return console.log(board);
+        return console.log(board), players.switchTurn();
     };
 
-    return {get, pick};
+    return {print, pick, cellValue};
 })();
+
+// I need to add the logic to pick that you cannot pick an already selected cell
+
+// I then need to add a win condition and checking after each pick
 
 const players = (function () {
     let playerOne = 'Player One';
     let playerTwo = 'Player Two';
+    let token = 0;
+    let activePlayer = playerOne;
 
-    const activePlayer = playerOne;
     console.log(`It is ${activePlayer}'s turn.`)
 
-    let token = 0;
-    if (activePlayer === playerOne) {
+    const getToken = () => {if (activePlayer === playerOne) {
         token = 1;
     } else if (activePlayer === playerTwo) {
         token = 2;
-    };
-    const getToken = () => token;
+    }
+    return token};
+    // this getToken might be moved to gameController together with a getValue function
 
     const getActivePlayer = () => console.log(activePlayer);
     const switchTurn = () => {activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
@@ -32,3 +39,5 @@ const players = (function () {
 
     return {getToken, getActivePlayer, switchTurn}
 })();
+
+// board.pick(2,2);
