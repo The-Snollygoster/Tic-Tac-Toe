f309fb70481abedc4e14440519d2577ad7f54af6
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-04-02 00:22:42 +0100

    Win check and clean up
    I've completed the part of the win check function
    that will detect if a row is winning. I still need
    to implement columns and diagonals.
    
    I've also deleted several lines of redundant code
    such as defining cell in board and shorthanding
    the if statements for switchTurn and getToken.

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
 const board = (function () {
-    const gameBoard = [[1,2,3],[4,5,6],[7,8,9]];
+    const gameBoard = [[0,0,0],[0,0,0],[0,0,0]];
     const getBoard = () => gameBoard;
     const print = () => console.log(gameBoard);
-    const cell = (row, column) => {return gameBoard[row-1][column-1]};
     
     const pick = (row, column) => {
-        if (cell(row,column) === 0) {
+        if (gameBoard[row-1][column-1] === 0) {
             gameBoard[(row-1)].splice((column-1), 1, game.getToken());
+            // game.winCheck();
             game.switchTurn();
         } else {
             console.log('Space already taken, please choose another!');
-        }
+        };
         return console.log(gameBoard)
     };
 
-    return {getBoard, print, pick, cell};
+    return {getBoard, print, pick};
 })();
 
-// I then need to add a win condition and checking after each pick
-
 const game = (function () {
-    const players = [{name: 'Player One', token: 'X', winArray: []}, {name: 'Player Two', token: 'O', winArray: []}];
+    const players = [{name: 'Player One', token: 1}, {name: 'Player Two', token: 2}];
     let activePlayer = players[0];
     const getActivePlayer = () => console.log(activePlayer.name);
     const playerNameChange = (playerIndex, name) => players[(playerIndex-1)].name = name;
-
-    const getToken = () => {
-        if (activePlayer === players[0]) {
-            return players[0].token;
-        } else if (activePlayer === players[1]) {
-            return players[1].token;
-        }
-    };
-
-    const switchTurn = () => {activePlayer === players[0] ? activePlayer = players[1] : activePlayer = players[0];
-        console.log(`It is ${activePlayer.name}'s turn.`)
-        };
-
+    const getToken = () => activePlayer === players[0] ? players[0].token : players[1].token;
+    const switchTurn = () => {activePlayer === players[0] ? activePlayer = players[1] : activePlayer = players[0]; console.log(`It is ${activePlayer.name}'s turn.`)};
     const playRound = () => { return console.log(board.print()), console.log(`It is ${activePlayer.name}'s turn.`)};
 
-    function sameCheck (value) {
-        return value === 1;
-    };
-
+    // Win Checking (only checked for winning rows so far)
     const winCheck = () => {
         let current = board.getBoard();
-        if (current[1].every(sameCheck)) {
-            console.log('We have a winner!')
+        if (current[0].every((value) => value === 1) || current[1].every((value) => value === 1) || current[2].every((value) => value === 1)) {
+            console.log(`We have a winner! Congratulations ${players[0].name}`)
+        } else if (current[0].every((value) => value === 2) || current[1].every((value) => value === 2) || current[2].every((value) => value === 2)) {
+            console.log(`We have a winner! Congratulations ${players[1].name}`)
         } else {
-            console.log('No winner yet')
-        }
+            console.log('No winner yet!')
+        };
     };
 
     return {playerNameChange, getToken, getActivePlayer, switchTurn, playRound, winCheck}
 })();
+
+game.playRound();
 
 // This will be my function that renders the game into HTML/DOM.
 const render = (function () {
@@ -62,6 +50,4 @@ const render = (function () {
 
 // board.pick(2,2);
 // game.playerNameChange(1, '')
-
-// I could number every cell on the board. Then take the spliced number from the pick function and push it into an array each player has. Then, check if the 
-// combination of numbers the player has equals one of the winning combination of cells.
+// game.winCheck()
