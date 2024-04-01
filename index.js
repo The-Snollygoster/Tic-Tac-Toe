e36e5c872a8a43214b30df3a04f43856249e0d9e
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-04-01 18:50:08 +0100

    Created player turn
    I've created the player factory that holds both
    players, both their tokens and the ability to
    switch turn.
    
    The game will now start on player ones turn and
    you can select a cell and replace it with the
    current players token.

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-// Ok. I'm using a factory function to create an object that holds the board as well as methods I can us on the board.
-// I've also wrapped the factory in an IIFE so it isn't global or reusable.
 const board = (function () {
     const board = [[0,0,0],[0,0,0],[0,0,0]];
-    // console.log(board);
-
-    const playerToken = 1;
 
     const get = () => console.log(board);
 
-    const pick = (row, column) => board[(row-1)].splice((column-1), 1, playerToken);
+    const pick = (row, column) => {board[(row-1)].splice((column-1), 1, players.getToken());
+        return console.log(board);
+    };
 
     return {get, pick};
 })();
 
-// const playerOne = '1';
-// const playerTwo = '2';
+const players = (function () {
+    let playerOne = 'Player One';
+    let playerTwo = 'Player Two';
 
-// function Players() {
+    const activePlayer = playerOne;
+    console.log(`It is ${activePlayer}'s turn.`)
 
-// };
-// I'll need players and a way of discerning who's turn it is and switching turns, also a 'game controller' 
+    let token = 0;
+    if (activePlayer === playerOne) {
+        token = 1;
+    } else if (activePlayer === playerTwo) {
+        token = 2;
+    };
+    const getToken = () => token;
+
+    const getActivePlayer = () => console.log(activePlayer);
+    const switchTurn = () => {activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
+        console.log(`It is ${activePlayer}'s turn.`)
+        };
+
+    return {getToken, getActivePlayer, switchTurn}
+})();
