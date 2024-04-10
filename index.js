b0fc59f33b6bb42821afd065a7f6d56da4068350
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-04-11 00:23:15 +0100

    Building event Listeners
    I've made the board and reset buttons transparent
    and only visible when the play button is pressed.
    I've also started on the cell event listeners that
    will be used to pick a certain cell by players. I
    Just need to figure out index passing first.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const playButton = document.querySelector('.play')
+const cell = document.querySelectorAll('.cell')
+
+playButton.addEventListener('click', () => {
+    game.playRound();
+});
+
+// currently trying to figure out how to add array index to each cell so that when a cell is clicked 
+// it corresponds to the correct array index on the board and invokes 'pick' with the correct passed index
+// pretty sure I can just use index in the forEach methods in the board render (which is always the second param)
+// to log the current index of the cell from the array and gift it to the cell as an ID? and then I'll take that 
+// ID contents and use it to pass into board.pick.
+cell.forEach(addEventListener('click', () => {
+    // board.pick(,);
+}));
+
 const board = (function () {
     const gameBoard = [[0,0,0],[0,0,0],[0,0,0]];
     const getDiag = () => [[gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]], [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]]];
@@ -28,7 +44,7 @@ const game = (function () {
     const playerNameChange = (playerIndex, name) => players[(playerIndex-1)].name = name;
     const getToken = () => activePlayer === players[0] ? players[0].token : players[1].token;
     const switchTurn = () => {activePlayer === players[0] ? activePlayer = players[1] : activePlayer = players[0]; console.log(`It is ${activePlayer.name}'s turn.`)};
-    const playRound = () => { return console.log(board.print()), console.log(`It is ${activePlayer.name}'s turn.`)};
+    const playRound = () => { return render.boardRender(), console.log(board.print()), console.log(`It is ${activePlayer.name}'s turn.`)};
 
     const winCheck = () => {
         let winningPlayer = '';
@@ -59,21 +75,27 @@ const game = (function () {
 // This will be my function that renders the game into HTML/DOM.
 const render = (function () {
     const container = document.querySelector('.container');
+    const reset = document.querySelector('.reset');
 
     const boardRender = () => {
         container.innerHTML = '';
+        container.setAttribute('style', 'opacity: 1;');
+        reset.setAttribute('style', 'opacity: 1;')
         board.getBoard().forEach(function(row) {
-            row.forEach((value) => {
+            row.forEach((value, index) => {
                 if (value === 1) {
                     const div = document.createElement('div');
                     div.innerHTML = '<img src="./icons/circle.svg" alt="Circle">';
+                    div.classList.add('cell');
                     container.appendChild(div);
                 } else if (value === 2) {
                     const div = document.createElement('div');
                     div.innerHTML = '<img src="./icons/x.svg" alt="Cross">';
+                    div.classList.add('cell');
                     container.appendChild(div);
                 } else {
                     const div = document.createElement('div');
+                    div.classList.add('cell');
                     container.appendChild(div);
                 };
             });
@@ -81,9 +103,6 @@ const render = (function () {
     };
     return {boardRender};
 })();
-
-game.playRound();
-render.boardRender();
 
 // board.pick(2,2);
 // game.playerNameChange(1, '')
